530976c264d7b7fcf8fd37d4e0c422d33f16beaf
martinkrbec <martin.krbec@dgfc.cz>
2026-04-27 15:52:43 +0200

    Bump to v 6.0

--- app.js
+++ app.js
@@ -5,7 +5,7 @@
    ============================================================ */
 'use strict';
 
-const APP_VERSION = 'printguard-5.1.0';
+const APP_VERSION = 'printguard-6.0.0';
 const DB_NAME     = 'printguard-db';
 const DB_VERSION  = 2;
 const ST_ITEMS    = 'items';
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "printguard-v5.1.0";
+const CACHE_NAME = "printguard-v6.0.0";
 
 self.addEventListener("install", (event) => {
   self.skipWaiting();
