a8ef4d169da481ee2059812861b6e0011918e5b0
martty98-star <martty98@gmail.com>
2026-05-04 01:03:44 +0200

    Invert DGFC badge in dark theme

--- app/config.js
+++ app/config.js
@@ -2,7 +2,7 @@
 'use strict';
 
 (function attachPrintGuardAppConfig(global) {
-  const APP_VERSION = 'printguard-7.0.1';
+  const APP_VERSION = 'printguard-7.0.2';
 
   function ls(k, v) {
     if (v !== undefined) { localStorage.setItem(k, String(v)); return v; }
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "printguard-v7.0.1";
+const CACHE_NAME = "printguard-v7.0.2";
 
 self.addEventListener("install", (event) => {
   self.skipWaiting();
